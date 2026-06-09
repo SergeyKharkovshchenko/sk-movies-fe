360380c75e8c612c3e9276e95ca34565d40eeb09
Sergey Kharkovshchenko <sergey.kharkovshchenko@pantopix.com>
2026-06-09 17:28:15 +0200

    fix

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -1,7 +1,9 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/svelte';
 import Page from './+page.svelte';
+
+vi.mock('$app/navigation', () => ({ goto: vi.fn() }));
 
 describe('/+page.svelte', () => {
 	test('should render page', () => {
