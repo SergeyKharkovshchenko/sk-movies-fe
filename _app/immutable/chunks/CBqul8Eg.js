01b921722d8a7de3d62522d7486ffe1519845d89
SergeyKharkovshchenko <SergeyKharkovshchenko@users.noreply.github.com>
2026-06-12 11:58:14 +0000

    Deploy main to gh-pages

---
+++ _app/immutable/chunks/CBqul8Eg.js
@@ -0,0 +1,4 @@
+import"./Bzak7iHL.js";import{p as f,e as b,s as u,r as _,t as o,a as d,b as v,d as s}from"./DosNBlVh.js";import{a as h}from"./Cw_Ae9jG.js";import{s as p}from"./BlLNLKaA.js";import{i as g}from"./DZxR4Aob.js";import{s as l}from"./Ffb3YQ6f.js";var k=s("<i></i>"),x=s("<button><!> <!></button>");function q(m,a){f(a,!0);var e=x();e.__click=()=>{a.disabled||a.handleClick()};var t=b(e);{var c=i=>{var r=k();o(()=>l(r,1,`mr-4 text-xl fa-solid ${a.icon??""}`)),d(i,r)};g(t,i=>{a.icon&&i(c)})}var n=u(t,2);p(n,()=>a.children),_(e),o(()=>l(e,1,`p-2 mb-2 w-full text-left rounded-md disabled:opacity-50 disabled:border disabled:border-gray-500
+	${a.class??""} 
+	
+	${a.isSelected?"bg-primary-950":""}`)),d(m,e),v()}h(["click"]);export{q as H};
