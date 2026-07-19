054c58ee1baa9a9833d328252c0ae218ea368cb2
Sergey Kharkovshchenko <sergey.kharkovshchenko@pantopix.com>
2026-07-19 14:55:05 +0200

    (fix): lint

--- src/lib/utils/dnd.js
+++ src/lib/utils/dnd.js
@@ -7,106 +7,106 @@
  * @param {any} node
  * @param {any} data
  */
-export function draggable(node, data) {
-	let state = data;
+export function draggable (node, data) {
+  let state = data
 
-	node.draggable = true;
-	node.style.cursor = 'grab';
+  node.draggable = true
+  node.style.cursor = 'grab'
 
-	/**
-	 * @param {any} e
-	 */
-	function handleDragstart(e) {
-		if (!e.dataTransfer) return;
-		e.dataTransfer.setData('text/plain', state);
-	}
+  /**
+   * @param {any} e
+   */
+  function handleDragstart (e) {
+    if (!e.dataTransfer) return
+    e.dataTransfer.setData('text/plain', state)
+  }
 
-	node.addEventListener('dragstart', handleDragstart);
+  node.addEventListener('dragstart', handleDragstart)
 
-	return {
-		/**
-		 * @param {any} data
-		 */
-		update(data) {
-			state = data;
-		},
+  return {
+    /**
+     * @param {any} data
+     */
+    update (data) {
+      state = data
+    },
 
-		destroy() {
-			node.removeEventListener('dragstart', handleDragstart);
-		}
-	};
+    destroy () {
+      node.removeEventListener('dragstart', handleDragstart)
+    }
+  }
 }
 
 /**
  * @param {any} node
  * @param {any} options
  */
-export function dropzone(node, options) {
-	let state = {
-		dropEffect: 'move',
-		dragover_class: 'droppable',
-		...options
-	};
+export function dropzone (node, options) {
+  let state = {
+    dropEffect: 'move',
+    dragover_class: 'droppable',
+    ...options
+  }
 
-	/**
-	 * @param {any} e
-	 */
-	function handleDragenter(e) {
-		if (!(e.target instanceof HTMLElement)) return;
-		e.target.classList.add(state.dragover_class);
-	}
+  /**
+   * @param {any} e
+   */
+  function handleDragenter (e) {
+    if (!(e.target instanceof HTMLElement)) return
+    e.target.classList.add(state.dragover_class)
+  }
 
-	/**
-	 * @param {any} e
-	 */
-	function handleDragleave(e) {
-		if (!(e.target instanceof HTMLElement)) return;
-		e.target.classList.remove(state.dragover_class);
-	}
+  /**
+   * @param {any} e
+   */
+  function handleDragleave (e) {
+    if (!(e.target instanceof HTMLElement)) return
+    e.target.classList.remove(state.dragover_class)
+  }
 
-	/**
-	 * @param {any} e
-	 */
-	function handleDragover(e) {
-		e.preventDefault();
-		if (!e.dataTransfer) return;
-		e.dataTransfer.dropEffect = state.dropEffect;
-	}
+  /**
+   * @param {any} e
+   */
+  function handleDragover (e) {
+    e.preventDefault()
+    if (!e.dataTransfer) return
+    e.dataTransfer.dropEffect = state.dropEffect
+  }
 
-	/**
-	 * @param {any} e
-	 */
-	function handleDrop(e) {
-		e.preventDefault();
-		if (!e.dataTransfer) return;
-		const data = e.dataTransfer.getData('text/plain');
-		if (!(e.target instanceof HTMLElement)) return;
-		e.target.classList.remove(state.dragover_class);
-		state.on_dropzone(data, e);
-	}
+  /**
+   * @param {any} e
+   */
+  function handleDrop (e) {
+    e.preventDefault()
+    if (!e.dataTransfer) return
+    const data = e.dataTransfer.getData('text/plain')
+    if (!(e.target instanceof HTMLElement)) return
+    e.target.classList.remove(state.dragover_class)
+    state.on_dropzone(data, e)
+  }
 
-	node.addEventListener('dragenter', handleDragenter);
-	node.addEventListener('dragleave', handleDragleave);
-	node.addEventListener('dragover', handleDragover);
-	node.addEventListener('drop', handleDrop);
+  node.addEventListener('dragenter', handleDragenter)
+  node.addEventListener('dragleave', handleDragleave)
+  node.addEventListener('dragover', handleDragover)
+  node.addEventListener('drop', handleDrop)
 
-	return {
-		/**
-		 * @param {any} options
-		 */
-		update(options) {
-			state = {
-				dropEffect: 'move',
-				dragover_class: 'droppable',
-				...options
-			};
-		},
+  return {
+    /**
+     * @param {any} options
+     */
+    update (options) {
+      state = {
+        dropEffect: 'move',
+        dragover_class: 'droppable',
+        ...options
+      }
+    },
 
-		destroy() {
-			node.removeEventListener('dragenter', handleDragenter);
-			node.removeEventListener('dragleave', handleDragleave);
-			node.removeEventListener('dragover', handleDragover);
-			node.removeEventListener('drop', handleDrop);
-		}
-	};
+    destroy () {
+      node.removeEventListener('dragenter', handleDragenter)
+      node.removeEventListener('dragleave', handleDragleave)
+      node.removeEventListener('dragover', handleDragover)
+      node.removeEventListener('drop', handleDrop)
+    }
+  }
 }
