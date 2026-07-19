2d773dc05f96ecb9845110cd582b975958719022
SergeyKharkovshchenko <SergeyKharkovshchenko@users.noreply.github.com>
2026-07-19 12:57:35 +0000

    Deploy main to gh-pages

---
+++ _app/immutable/chunks/BsJ-rDJa.js
@@ -0,0 +1,1 @@
+import{w as t}from"./DJMh-u47.js";import{g as o,b as r}from"./C0bw2VdO.js";const c=t("content"),n=t(!0),i=t("/"),b=e=>o(`${r}${e}`);export{c as a,i as c,b as g,n as t};
--- _app/immutable/chunks/D3RLJu6w.js
+++
@@ -1,4 +0,0 @@
-import"./Bzak7iHL.js";import{p as f,c as b,s as u,r as _,t as o,a as d,b as v,f as s}from"./BtOGSnnR.js";import{a as h}from"./BI5MG2Ix.js";import{s as p}from"./CMEjEk8q.js";import{i as g}from"./BY1xrx7N.js";import{s as l}from"./DKgzPocX.js";var k=s("<i></i>"),x=s("<button><!> <!></button>");function q(c,a){f(a,!0);var e=x();e.__click=()=>{a.disabled||a.handleClick()};var t=b(e);{var m=i=>{var r=k();o(()=>l(r,1,`mr-4 text-xl fa-solid ${a.icon??""}`)),d(i,r)};g(t,i=>{a.icon&&i(m)})}var n=u(t,2);p(n,()=>a.children),_(e),o(()=>l(e,1,`p-2 mb-2 w-full text-left rounded-md disabled:opacity-50 disabled:border disabled:border-gray-500
-	${a.class??""} 
-	
-	${a.isSelected?"bg-primary-950":""}`)),d(c,e),v()}h(["click"]);export{q as H};
